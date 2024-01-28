76b8e8b29d948e6fcb7a8048e984e0a12a91ad4a
stephen <u6269649@anu.edu.au>
2024-01-28 21:50:09 +1100

    BUILD: reading brainfuck file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::env;
+use std::fs;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        eprintln!("error: No file path provided.\nPlease specify the path to a Brainfuck file.\nUsage: oxybrain <path_to_brainfuck_file>");
+        return;
+    }
+
+    let file_path = &args[1];
+
+    if !Path::new(file_path).exists() {
+        eprintln!("error: the brainfuck file does not exist.");
+        return;
+    }
+
+    let code = fs::read_to_string(file_path).expect("error reading the brainfuck file");
+
+    println!("got brainfuck code:\n{}", code);
 }
